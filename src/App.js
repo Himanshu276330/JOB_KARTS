383f53bac039936de3720fc8598f8dda68ae7db0
atharva-gadekar <atharvagadekar@gmail.com>
2022-03-15 01:41:22 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import Header from "./component/header";
 import Main from "./component/main";
 import Search from "./component/search";
-import footer from "./component/header";
+import Footer from "./component/footer";
 
 function App() {
   return(
     <div>
       <Header/>
       <Main/>
+      <Footer/>
     </div>
   );
 }
--- src/component/footer.js
+++ src/component/footer.js
@@ -1,7 +1,54 @@
 import React from "react";
+import ln from '../resources/linkedin.png'
 
 function Footer() {
     return (
-        <></>
+        <div className="footer">
+        <div className="footerp1">
+            <div className="sec1">
+                <div className="header">
+                    About Us
+                </div>
+
+                <div className="footerpage"><a href="">How it works</a></div>
+                <div className="footerpage"><a href="">Careers</a></div>
+                <div className="footerpage"><a href="">Terms of Service</a></div>
+            </div>
+            <div className="sec2">
+                <div className="header">
+                    Contact Us
+                </div>
+
+                <div className="footerpage"><a href="">Contact</a></div>
+                <div className="footerpage"><a href="">Support</a></div>
+            </div>
+            <div className="sec3">
+                <div className="header">
+                    Social Media
+                </div>
+
+                <div className="footerpage"><a href="">Instagram</a></div>
+                <div className="footerpage"><a href="">Facebook</a></div>
+                <div className="footerpage"><a href="">Youtube</a></div>
+                <div className="footerpage"><a href="">Twitter</a></div>
+            </div>
+        </div>
+        <div className="footerp2">
+        <div className="logoname">
+          JOBSKART
+        </div>
+
+        <div className="others">
+          <div className="inner">
+          <div className="item"><div className="img"><a href=""><img src={ln} class="social" /></a></div><div className="name">Anurag</div></div>
+          <div className="item"><div className="img"><a href=""><img src={ln} class="social" /></a></div><div className="name">Atharva</div></div>
+          <div className="item"><div className="img"><a href=""><img src={ln} class="social" /></a></div><div className="name">Himanshu</div></div>
+          <div className="item"><div className="img"><a href=""><img src={ln} class="social" /></a></div><div className="name">Manas</div></div>
+          </div>
+        </div>
+      </div>
+        </div>
     );
 }
+
+export default Footer;
